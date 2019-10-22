d210b18296ed482c05e5c0b88fce4f2ad806e384
Bogdan Bodnar <bogdan.bodnar@nic.cz>
2019-10-22 14:28:24 +0200

    Bump version 0.2.0.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ class CustomBuild(build_py):
 
 setuptools.setup(
     name=NAME,
-    version='0.1.0',
+    version='0.2.0',
     packages=setuptools.find_packages(exclude=['tests']),
     include_package_data=True,
 

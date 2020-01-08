24c414a3be944879644f78105dd993bef7c5a64b
Bogdan Bodnar <bogdan.bodnar@nic.cz>
2020-01-08 11:04:36 +0100

    Remove unnecessary test utils.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,1 @@
-import pytest
-
-from reforis_openvpn import blueprint
-
-
 pytest_plugins = 'reforis.test_utils.fixtures'
-
-
-@pytest.fixture(scope='module')
-def app(app_with_blueprint):
-    return app_with_blueprint(blueprint)

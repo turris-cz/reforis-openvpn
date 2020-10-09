5d6617836df44963b69a78d8adcc495955697b5b
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2020-10-09 13:06:29 +0200

    Bump v1.4.0
    * Add fluid layout support (redesign)
    * Restructure headings
    * Add cursor pointer on hover file input
    * Integrate ESLint + Prettier + reForis styleguide
    * Use ForisJS v5.1.5
    * NPM audit fix

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ class CustomBuild(build_py):
 
 setuptools.setup(
     name=NAME,
-    version='1.3.1',
+    version='1.4.0',
     packages=setuptools.find_packages(exclude=['tests']),
     include_package_data=True,
 
